176375571167e484d7982856ef2693ffe8c1c8c9
Hans-Kristian Arntzen <post@themaister.net>
2023-10-22 23:23:22 +0200

    Fix Win32 build.

--- tcp_socket.cpp
+++ tcp_socket.cpp
@@ -1,20 +1,35 @@
 #include "tcp_socket.hpp"
 
+#ifdef _WIN32
+#include <ws2tcpip.h>
+#include <winsock2.h>
+#else
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <unistd.h>
+#define closesocket(x) ::close(x)
+#endif
 
 namespace PyroFling
 {
 TCPReader::~TCPReader()
 {
 	if (fd >= 0)
-		::close(fd);
+		closesocket(fd);
 }
 
 bool TCPReader::connect(const char *addr, const char *port)
 {
+#ifdef _WIN32
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		LOGE("Failed to initialize WSA.\n");
+		return false;
+	}
+#endif
+
 	addrinfo hints = {};
 	addrinfo *servinfo;
 
@@ -32,7 +47,10 @@ bool TCPReader::connect(const char *addr, const char *port)
 		if (new_fd < 0)
 			return false;
 		if (::connect(new_fd, walk->ai_addr, walk->ai_addrlen) < 0)
+		{
+			closesocket(new_fd);
 			continue;
+		}
 		fd = new_fd;
 		break;
 	}
@@ -51,8 +69,8 @@ bool TCPReader::read(void *data_, size_t size)
 
 	while (size)
 	{
-		ssize_t ret;
-		if ((ret = ::recv(fd, data, size, 0)) <= 0)
+		int ret;
+		if ((ret = ::recv(fd, reinterpret_cast<char *>(data), size, 0)) <= 0)
 			return false;
 		size -= ret;
 		data += ret;
