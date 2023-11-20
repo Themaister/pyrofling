518659ddf403c0335f850935f4f95fc83ec307e9
Hans-Kristian Arntzen <post@themaister.net>
2023-11-20 21:47:15 +0100

    Update Granite.

--- pyrofling_gamepad.cpp
+++ pyrofling_gamepad.cpp
@@ -20,106 +20,81 @@ static void print_help()
 	LOGI("pyrofling-gamepad ip:port\n");
 }
 
-static void poll_thread_main(PyroFling::PyroStreamClient &pyro, InputTracker &tracker)
+struct PadHandler : Granite::InputTrackerHandler
 {
-	struct PadHandler : Granite::InputTrackerHandler
+	PyroFling::PyroStreamClient *pyro = nullptr;
+	void dispatch(const Granite::TouchDownEvent &) override {}
+	void dispatch(const Granite::TouchUpEvent &) override {}
+	void dispatch(const Granite::TouchGestureEvent &) override {}
+	void dispatch(const Granite::JoypadButtonEvent &) override {}
+	void dispatch(const Granite::JoypadAxisEvent &) override {}
+	void dispatch(const Granite::KeyboardEvent &) override {}
+	void dispatch(const Granite::OrientationEvent &) override {}
+	void dispatch(const Granite::MouseButtonEvent &) override {}
+	void dispatch(const Granite::MouseMoveEvent &) override {}
+	void dispatch(const Granite::InputStateEvent &) override {}
+	void dispatch(const Granite::JoypadConnectionEvent &) override {}
+	void dispatch(const Granite::JoypadStateEvent &e) override
 	{
-		PyroFling::PyroStreamClient *pyro = nullptr;
-		void dispatch(const Granite::TouchDownEvent &) override {}
-		void dispatch(const Granite::TouchUpEvent &) override {}
-		void dispatch(const Granite::TouchGestureEvent &) override {}
-		void dispatch(const Granite::JoypadButtonEvent &) override {}
-		void dispatch(const Granite::JoypadAxisEvent &) override {}
-		void dispatch(const Granite::KeyboardEvent &) override {}
-		void dispatch(const Granite::OrientationEvent &) override {}
-		void dispatch(const Granite::MouseButtonEvent &) override {}
-		void dispatch(const Granite::MouseMoveEvent &) override {}
-		void dispatch(const Granite::InputStateEvent &) override {}
-		void dispatch(const Granite::JoypadConnectionEvent &) override {}
-		void dispatch(const Granite::JoypadStateEvent &e) override
+		using namespace Granite;
+
+		pyro_gamepad_state state = {};
+		bool done = false;
+
+		for (unsigned i = 0, n = e.get_num_indices(); i < n && !done; i++)
 		{
-			using namespace Granite;
+			if (!e.is_connected(i))
+				continue;
 
-			pyro_gamepad_state state = {};
-			bool done = false;
+			auto &joy = e.get_state(i);
 
-			for (unsigned i = 0, n = e.get_num_indices(); i < n && !done; i++)
-			{
-				if (!e.is_connected(i))
-					continue;
+			// Don't cause feedbacks when used locally.
+			if (joy.vid == PyroFling::VirtualGamepad::FAKE_VID &&
+			    joy.pid == PyroFling::VirtualGamepad::FAKE_PID)
+				continue;
 
-				auto &joy = e.get_state(i);
+			state.axis_lx = int16_t(0x7fff * joy.axis[int(JoypadAxis::LeftX)]);
+			state.axis_ly = int16_t(0x7fff * joy.axis[int(JoypadAxis::LeftY)]);
+			state.axis_rx = int16_t(0x7fff * joy.axis[int(JoypadAxis::RightX)]);
+			state.axis_ry = int16_t(0x7fff * joy.axis[int(JoypadAxis::RightY)]);
+			state.hat_x += (joy.button_mask & (1 << int(JoypadKey::Left))) != 0 ? -1 : 0;
+			state.hat_x += (joy.button_mask & (1 << int(JoypadKey::Right))) != 0 ? +1 : 0;
+			state.hat_y += (joy.button_mask & (1 << int(JoypadKey::Up))) != 0 ? -1 : 0;
+			state.hat_y += (joy.button_mask & (1 << int(JoypadKey::Down))) != 0 ? +1 : 0;
+			state.lz = uint8_t(255.0f * joy.axis[int(JoypadAxis::LeftTrigger)]);
+			state.rz = uint8_t(255.0f * joy.axis[int(JoypadAxis::RightTrigger)]);
+			if (joy.button_mask & (1 << int(JoypadKey::East)))
+				state.buttons |= PYRO_PAD_EAST_BIT;
+			if (joy.button_mask & (1 << int(JoypadKey::South)))
+				state.buttons |= PYRO_PAD_SOUTH_BIT;
+			if (joy.button_mask & (1 << int(JoypadKey::West)))
+				state.buttons |= PYRO_PAD_WEST_BIT;
+			if (joy.button_mask & (1 << int(JoypadKey::North)))
+				state.buttons |= PYRO_PAD_NORTH_BIT;
+			if (joy.button_mask & (1 << int(JoypadKey::LeftShoulder)))
+				state.buttons |= PYRO_PAD_TL_BIT;
+			if (joy.button_mask & (1 << int(JoypadKey::RightShoulder)))
+				state.buttons |= PYRO_PAD_TR_BIT;
+			if (joy.button_mask & (1 << int(JoypadKey::LeftThumb)))
+				state.buttons |= PYRO_PAD_THUMBL_BIT;
+			if (joy.button_mask & (1 << int(JoypadKey::RightThumb)))
+				state.buttons |= PYRO_PAD_THUMBR_BIT;
+			if (joy.button_mask & (1 << int(JoypadKey::Start)))
+				state.buttons |= PYRO_PAD_START_BIT;
+			if (joy.button_mask & (1 << int(JoypadKey::Select)))
+				state.buttons |= PYRO_PAD_SELECT_BIT;
+			if (joy.button_mask & (1 << int(JoypadKey::Mode)))
+				state.buttons |= PYRO_PAD_MODE_BIT;
 
-				// Don't cause feedbacks when used locally.
-				if (joy.vid == PyroFling::VirtualGamepad::FAKE_VID &&
-				    joy.pid == PyroFling::VirtualGamepad::FAKE_PID)
-					continue;
-
-				state.axis_lx = int16_t(0x7fff * joy.axis[int(JoypadAxis::LeftX)]);
-				state.axis_ly = int16_t(0x7fff * joy.axis[int(JoypadAxis::LeftY)]);
-				state.axis_rx = int16_t(0x7fff * joy.axis[int(JoypadAxis::RightX)]);
-				state.axis_ry = int16_t(0x7fff * joy.axis[int(JoypadAxis::RightY)]);
-				state.hat_x += (joy.button_mask & (1 << int(JoypadKey::Left))) != 0 ? -1 : 0;
-				state.hat_x += (joy.button_mask & (1 << int(JoypadKey::Right))) != 0 ? +1 : 0;
-				state.hat_y += (joy.button_mask & (1 << int(JoypadKey::Up))) != 0 ? -1 : 0;
-				state.hat_y += (joy.button_mask & (1 << int(JoypadKey::Down))) != 0 ? +1 : 0;
-				state.lz = uint8_t(255.0f * joy.axis[int(JoypadAxis::LeftTrigger)]);
-				state.rz = uint8_t(255.0f * joy.axis[int(JoypadAxis::RightTrigger)]);
-				if (joy.button_mask & (1 << int(JoypadKey::East)))
-					state.buttons |= PYRO_PAD_EAST_BIT;
-				if (joy.button_mask & (1 << int(JoypadKey::South)))
-					state.buttons |= PYRO_PAD_SOUTH_BIT;
-				if (joy.button_mask & (1 << int(JoypadKey::West)))
-					state.buttons |= PYRO_PAD_WEST_BIT;
-				if (joy.button_mask & (1 << int(JoypadKey::North)))
-					state.buttons |= PYRO_PAD_NORTH_BIT;
-				if (joy.button_mask & (1 << int(JoypadKey::LeftShoulder)))
-					state.buttons |= PYRO_PAD_TL_BIT;
-				if (joy.button_mask & (1 << int(JoypadKey::RightShoulder)))
-					state.buttons |= PYRO_PAD_TR_BIT;
-				if (joy.button_mask & (1 << int(JoypadKey::LeftThumb)))
-					state.buttons |= PYRO_PAD_THUMBL_BIT;
-				if (joy.button_mask & (1 << int(JoypadKey::RightThumb)))
-					state.buttons |= PYRO_PAD_THUMBR_BIT;
-				if (joy.button_mask & (1 << int(JoypadKey::Start)))
-					state.buttons |= PYRO_PAD_START_BIT;
-				if (joy.button_mask & (1 << int(JoypadKey::Select)))
-					state.buttons |= PYRO_PAD_SELECT_BIT;
-				if (joy.button_mask & (1 << int(JoypadKey::Mode)))
-					state.buttons |= PYRO_PAD_MODE_BIT;
-
-				done = true;
-			}
-
-			if (!pyro->send_gamepad_state(state))
-				dead = true;
+			done = true;
 		}
 
-		bool dead = false;
-	};
-
-	PadHandler handler;
-	handler.pyro = &pyro;
-
-#ifdef _WIN32
-	timeBeginPeriod(1);
-#endif
-
-	while (!handler.dead)
-	{
-		std::this_thread::sleep_for(std::chrono::milliseconds(4));
-		std::lock_guard<std::mutex> holder{tracker.get_lock()};
-		tracker.dispatch_current_state(0.0, &handler);
+		if (!pyro->send_gamepad_state(state))
+			dead = true;
 	}
 
-	SDL_Event event = {};
-	event.type = SDL_EVENT_QUIT;
-	SDL_PushEvent(&event);
-
-#ifdef _WIN32
-	timeEndPeriod(1);
-#endif
-}
+	bool dead = false;
+};
 
 int main(int argc, char **argv)
 {
@@ -179,6 +154,9 @@ int main(int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
+	SDL_SetHint(SDL_HINT_JOYSTICK_ALLOW_BACKGROUND_EVENTS, "1");
+	SDL_SetHint(SDL_HINT_AUTO_UPDATE_JOYSTICKS, "0");
+
 	const auto dispatcher = [&tracker](std::function<void ()> func) {
 		std::lock_guard<std::mutex> holder{tracker.get_lock()};
 		func();
@@ -187,18 +165,29 @@ int main(int argc, char **argv)
 	if (!pad.init(tracker, dispatcher))
 		return EXIT_FAILURE;
 
-	std::thread thr{[&pyro, &tracker]() {
-		poll_thread_main(pyro, tracker);
-	}};
+#ifdef _WIN32
+	timeBeginPeriod(1);
+#endif
 
-	// Should keep going until killed.
-	SDL_Event e;
-	while (SDL_WaitEvent(&e))
+	PadHandler handler;
+	handler.pyro = &pyro;
+
+	while (!handler.dead)
 	{
-		if (e.type == SDL_EVENT_QUIT)
-			break;
-		pad.process_sdl_event(e, tracker, dispatcher);
+		std::this_thread::sleep_for(std::chrono::milliseconds(4));
+		pad.update(tracker);
+		tracker.dispatch_current_state(0.0, &handler);
+
+		SDL_Event e;
+		while (SDL_PollEvent(&e))
+		{
+			if (e.type == SDL_EVENT_QUIT)
+				handler.dead = true;
+			pad.process_sdl_event(e, tracker, dispatcher);
+		}
 	}
 
-	thr.join();
+#ifdef _WIN32
+	timeEndPeriod(1);
+#endif
 }
