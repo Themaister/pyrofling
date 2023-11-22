ac1507600f4010e1245da1d2423f7c4d86253dfd
Hans-Kristian Arntzen <post@themaister.net>
2023-11-22 01:09:58 +0100

    Add FFmpeg as submodule and add scripts to build it.
    Prebuilt binary from msys2 seems to segfault for some reason in Vulkan video, but self-compiled does not ...

---
+++ build-ffmpeg-msys2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+export CFLAGS="-static -static-libgcc -static-libstdc++"
+
+mkdir -p ffmpeg-build-msys2
+cd ffmpeg-build-msys2
+
+../ffmpeg/configure --enable-static --disable-shared --prefix=output \
+	--disable-sdl2 \
+       	--enable-vulkan \
+       	--disable-vaapi \
+       	--disable-cuda \
+       	--disable-bzlib \
+       	--disable-iconv \
+       	--disable-lzma \
+       	--disable-zlib \
+	--disable-ffmpeg \
+	--disable-ffplay
+
+make -j$(nproc) install
+
