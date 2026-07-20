632fd7e57c121ab3f300f841c004e28c67d0633f
Hans-Kristian Arntzen <post@themaister.net>
2026-07-20 12:12:58 +0200

    Add helper scripts to build static FFmpeg with cross-mingw.

---
+++ build-ffmpeg-mingw-shared.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+export CFLAGS="-static -static-libgcc -static-libstdc++"
+
+mkdir -p ffmpeg-build-msys2
+cd ffmpeg-build-msys2
+
+../ffmpeg/configure --arch=x86_64 --target-os=mingw32 --cross-prefix=x86_64-w64-mingw32- \
+	--disable-static --enable-shared --prefix="$(pwd)/output" \
+	--disable-sdl2 \
+	--enable-vulkan \
+	--disable-vaapi \
+	--disable-cuda \
+	--disable-bzlib \
+	--disable-iconv \
+	--disable-lzma \
+	--disable-zlib \
+	--disable-ffmpeg \
+	--disable-ffprobe \
+	--disable-ffplay
+
+make -j$(nproc) install
+
